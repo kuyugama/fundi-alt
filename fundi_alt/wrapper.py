28a46b3ed587edbd5b1bcfa0cff8b7558a34c9d5
Kuyugama <mail.kuyugama@gmail.com>
2025-06-15 17:35:33 +0300

    Add ObjectWrapper

--- fundi_alt/wrapper.py
+++ fundi_alt/wrapper.py
@@ -5,6 +5,7 @@ import functools
 from fundi import from_
 
 T = typing.TypeVar("T")
+Self = typing.TypeVar("Self")
 
 Returns = typing.Callable[..., T]
 Decorator = typing.Callable[[Returns[T]], Returns[T]]
@@ -58,3 +59,51 @@ def resolver_wrapper(
     setattr(dependency, "__signature__", signature)
 
     return dependency
+
+
+class ObjectWrapper(typing.Generic[T]):
+    """
+    Wrapper for objects, useful when need distinguish two instances of the same class by their type
+
+    Usage::
+        class SubjectUser(User, ObjectWrapper[User]): ...
+
+        class ObjectUser(User, ObjectWrapper[User]): ...
+
+        subject = SubjectUser.by(user)
+
+        object = ObjectUser.by(user)
+
+        if isinstance(subject, SubjectUser):
+            print("This is a subject user")
+        if isinstance(object, ObjectUser):
+            print("subject user appears to be object user")
+
+        if isinstance(object, ObjectUser):
+            print("This is a object user")
+        if isinstance(object, SubjectUser):
+            print("object user appears to be subject user")
+    """
+
+    def __init_subclass__(cls) -> None:
+        for base in type.mro(cls):
+            if base is not object and base.__module__ == "builtins":
+                raise ValueError("Cannot wrap builtin type")
+
+        return super().__init_subclass__()
+
+    @classmethod
+    def by(cls: type[Self], object_: T) -> Self:
+        instance = cls.__new__(cls)
+
+        if hasattr(object_, "__getstate__"):
+            state = getattr(object_, "__getstate__")()
+        else:
+            state = object_.__dict__
+
+        if hasattr(instance, "__setstate__"):
+            getattr(instance, "__setstate__")(state)
+        else:
+            instance.__dict__.update(state)
+
+        return instance
